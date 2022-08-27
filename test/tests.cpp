d43fd4d448c4988fb1acfe8ccfaa9e132876464c
Fabio Soldati <soldati@gmx.net>
2022-08-27 12:44:04 +0200

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,24 @@
 
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:  Fabio Soldati, peakfinder.org
+Flexible algorithm to parse strings containing various latitude/longitude formats.
+This is a C++ implementation of the javascript version [coordinate-parser](https://github.com/otto-dev/coordinate-parser).
+Credit: Originally developed for javascript by [otto-dev](https://github.com/otto-dev).
+*/
+
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.h"
 
